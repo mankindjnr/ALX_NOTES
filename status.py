6e7651662c8167bcb502c182e7fbcade003291d2
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-31 17:22:01 +0400

    rqest id

---
+++ status.py
@@ -0,0 +1,10 @@
+import requests
+
+req = requests.get("https://alx-intranet.hbtn.io/status")
+
+content_type = req.headers['Content-Type']
+if 'text' in content_type:
+    content_type = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(content_type)))
+    print("\t- content: {}".format(content_type))
